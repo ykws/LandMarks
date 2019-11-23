78328d857c4b08dff9649b330cc92bca2925bc3f
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2019-11-23 19:17:41 +0900

    Initial Commit

---
+++ LandMarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LandMarks
+//
+//  Created by KAWASHIMA Yoshiyuki on 2019/11/23.
+//  Copyright © 2019 Yoshiyuki Kawashima. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
