0373e974e4c095c1b70e6d40efd4ab7c57df1bcd
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2019-11-24 23:41:12 +0900

    Create a Custom Image View

---
+++ LandMarks/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  LandMarks
+//
+//  Created by KAWASHIMA Yoshiyuki on 2019/11/24.
+//  Copyright © 2019 Yoshiyuki Kawashima. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay(Circle().stroke(Color.gray, lineWidth: 4))
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
