d03bf905d1634bcc52def3853622441013fbca79
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2019-11-27 03:10:33 +0900

    Create the Row View

---
+++ LandMarks/Landmark Views/LandmarkRow.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkRow.swift
+//  LandMarks
+//
+//  Created by KAWASHIMA Yoshiyuki on 2019/11/27.
+//  Copyright © 2019 Yoshiyuki Kawashima. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarkData[0])
+    }
+}
