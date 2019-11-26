866911a39199ef796aa50249ba4530b971734e47
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2019-11-27 02:58:34 +0900

    Add base models

---
+++ LandMarks/Models/Landmark.swift
@@ -0,0 +1,41 @@
+//
+//  Landmark.swift
+//  LandMarks
+//
+//  Created by KAWASHIMA Yoshiyuki on 2019/11/27.
+//  Copyright © 2019 Yoshiyuki Kawashima. All rights reserved.
+//
+
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    fileprivate var imageName: String
+    fileprivate var coordinates: Coordinates
+    var state: String
+    var park: String
+    var category: Category
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
+    }
+    
+    enum Category: String, CaseIterable, Codable, Hashable {
+        case featured = "Featured"
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+    }
+}
+
+extension Landmark {
+    var image: Image {
+        ImageStore.shared.image(name: imageName)
+    }
+}
+
+struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
+}
