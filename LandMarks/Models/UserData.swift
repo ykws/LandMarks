664519785ed0562fc849b5739c538ee4f30bcce2
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2019-12-01 00:29:04 +0900

    Use an Observable Object for Storage

---
+++ LandMarks/Models/UserData.swift
@@ -0,0 +1,15 @@
+//
+//  UserData.swift
+//  LandMarks
+//
+//  Created by KAWASHIMA Yoshiyuki on 2019/12/01.
+//  Copyright © 2019 ykws. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: ObservableObject {
+    @Published var showFavoritesOnly = false
+    @Published var landmarks = landmarkData
+}
