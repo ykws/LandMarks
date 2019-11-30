c56b072a5e202c22c2970b02ade6d7c0acfb99ef
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2019-12-01 00:20:42 +0900

    Filter the List View

--- LandMarks/Landmark Views/LandmarkList.swift
+++ LandMarks/Landmark Views/LandmarkList.swift
@@ -9,11 +9,15 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State var showFavoritesOnly = false
+    
     var body: some View {
         NavigationView {
             List(landmarkData) { landmark in
-                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                    LandmarkRow(landmark: landmark)
+                if !self.showFavoritesOnly || landmark.isFavorite {
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
             .navigationBarTitle(Text("Landmarks"))
