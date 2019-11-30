7de8b0dafaa34d74db91d080e0d036d9134cc39d
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2019-11-30 23:59:16 +0900

    Mark the User's Favorite Landmarks

--- LandMarks/Models/Landmark.swift
+++ LandMarks/Models/Landmark.swift
@@ -17,6 +17,7 @@ struct Landmark: Hashable, Codable, Identifiable {
     var state: String
     var park: String
     var category: Category
+    var isFavorite: Bool = false
     
     var locationCoordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
