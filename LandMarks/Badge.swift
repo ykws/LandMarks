6077ff74cef9addb4da0932a31118c91435638d6
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2019-12-02 22:48:02 +0900

    Create a Badge View

---
+++ LandMarks/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  LandMarks
+//
+//  Created by KAWASHIMA Yoshiyuki on 2019/12/02.
+//  Copyright © 2019 ykws. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text("Badge")
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
