2431d4a3c5c49c41abcef18eb050195bc4589e5f
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2019-12-09 23:05:49 +0900

    Customize View Transitions

--- LandMarks/HikeView.swift
+++ LandMarks/HikeView.swift
@@ -8,6 +8,16 @@
 
 import SwiftUI
 
+extension AnyTransition {
+    static var moveAndFade: AnyTransition {
+        let insertion = AnyTransition.move(edge: .trailing)
+            .combined(with: .opacity)
+        let removal = AnyTransition.scale
+            .combined(with: .opacity)
+        return .asymmetric(insertion: insertion, removal: removal)
+    }
+}
+
 struct HikeView: View {
     var hike: Hike
     @State private var showDetail = false
@@ -42,6 +52,7 @@ struct HikeView: View {
             
             if showDetail {
                 HikeDetail(hike: hike)
+                    .transition(.moveAndFade)
             }
         }
     }
