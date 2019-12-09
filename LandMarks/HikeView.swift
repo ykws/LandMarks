6a25d738b1a86bdc2d9b7a484332a424bc63f280
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2019-12-09 22:59:32 +0900

    Animate the Effects of State Changes

--- LandMarks/HikeView.swift
+++ LandMarks/HikeView.swift
@@ -28,7 +28,9 @@ struct HikeView: View {
                 Spacer()
                 
                 Button(action: {
-                    self.showDetail.toggle()
+                    withAnimation {
+                        self.showDetail.toggle()
+                    }
                 }) {
                     Image(systemName: "chevron.right.circle")
                         .imageScale(.large)
