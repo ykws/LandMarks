91087e637bd9ec03f80145a8ec10453c793ef0f9
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2019-12-15 11:08:13 +0900

    Add Rows of Landmarks

---
+++ LandMarks/CategoryRow.swift
@@ -0,0 +1,41 @@
+//
+//  CategoryRow.swift
+//  LandMarks
+//
+//  Created by KAWASHIMA Yoshiyuki on 2019/12/15.
+//  Copyright © 2019 ykws. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(self.categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(self.items) { landmark in
+                        Text(landmark.name)
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryRow(
+            categoryName: landmarkData[0].category.rawValue,
+            items: Array(landmarkData.prefix(4))
+        )
+    }
+}
