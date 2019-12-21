ed991d5078e551d4385a89054544e013b306524d
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2019-12-21 09:17:39 +0900

    Add Profile Model

---
+++ LandMarks/Models/Profile.swift
@@ -0,0 +1,21 @@
+//
+//  Profile.swift
+//  LandMarks
+//
+//  Created by KAWASHIMA Yoshiyuki on 2019/12/21.
+//  Copyright © 2019 ykws. All rights reserved.
+//
+
+import SwiftUI
+
+struct Profile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Profile_Previews: PreviewProvider {
+    static var previews: some View {
+        Profile()
+    }
+}
