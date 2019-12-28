b9e24a74e3f4f825eb857905bb8149cf38838fe9
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2019-12-29 08:30:14 +0900

    Create the View Controller's Data Source

--- LandMarks/PageViewController.swift
+++ LandMarks/PageViewController.swift
@@ -12,15 +12,40 @@ import UIKit
 struct PageViewController: UIViewControllerRepresentable {
     var controllers: [UIViewController]
     
+    func makeCoordinator() -> PageViewController.Coordinator {
+        Coordinator(self)
+    }
+    
     func makeUIViewController(context: UIViewControllerRepresentableContext<PageViewController>) -> UIPageViewController {
         let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
             navigationOrientation: .horizontal)
+        pageViewController.dataSource = context.coordinator
         return pageViewController
     }
 
     func updateUIViewController(_ pageViewController: UIPageViewController, context: UIViewControllerRepresentableContext<PageViewController>) {
         pageViewController.setViewControllers(
             [controllers[0]], direction: .forward, animated: true)
+    }
+    
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
+        var parent: PageViewController
+        
+        init(_ pageViewController: PageViewController) {
+            self.parent = pageViewController
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            guard let index = parent.controllers.firstIndex(of: viewController) else { return nil }
+            if index == 0 { return parent.controllers.last }
+            return parent.controllers[index - 1]
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            guard let index = parent.controllers.firstIndex(of: viewController) else { return nil }
+            if index + 1 == parent.controllers.count { return parent.controllers.first }
+            return parent.controllers[index + 1]
+        }
     }
 }
